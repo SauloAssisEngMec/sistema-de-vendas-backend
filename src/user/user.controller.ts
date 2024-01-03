febf6f1d7a96370c78600b6df052540bc4be178f
saulo <sauloassis4123@gmail.com>
2024-01-03 17:36:29 -0300

    feat: Add user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,25 @@
-import {CreateUserDto}   from './dtos/createUser.dto';
-import { Body, Controller,  Post } from '@nestjs/common';
+
+
+import { Body, Controller,  Get,  Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+
+    constructor( private readonly userService: UserService ) {}
     @Post() 
-    async createUser( @Body() createUser : CreateUserDto) {
+    async createUser( @Body() createUser : CreateUserDto): Promise <User> {
         
-        return (createUser)
-    }}
+        return this.userService.createUser(createUser)
+    }
+
+    @Get()
+    async getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+}
+
 
 
